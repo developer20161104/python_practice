ee369c60794bbdaa2aa88fb36433b364bfec3d22
developer20161104 <cqputyang@163.com>
2019-10-31 16:25:39 +0800

    small fix and plugin implement

--- leetcode/remove_duplicates.py
+++ leetcode/remove_duplicates.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         lens = len(nums)
@@ -14,22 +15,22 @@ class Solution:
         while i < lens:
             # recording duplicate element
             k = 0
-            while i+k < lens and nums[i+k] == pre:
+            while i + k < lens and nums[i + k] == pre:
                 k += 1
             # avoid out ot index
-            if i+k >= lens:
+            if i + k >= lens:
                 break
             # filling element
-            nums[st] = nums[i+k]
+            nums[st] = nums[i + k]
             st += 1
-            pre = nums[i+k]
+            pre = nums[i + k]
             i += k
         # print(nums)
         return st
 
     def removeElement(self, nums: List[int], val: int) -> int:
         # k为多余的统计步骤
-        '''
+        """
         lens = len(nums)
         k, pos = 0, 0
         for i in range(lens):
@@ -41,7 +42,7 @@ class Solution:
 
         # print(nums)
         return lens-k
-        '''
+        """
         # find the position and insert into it directly
         lens = len(nums)
         pos = 0
@@ -49,28 +50,28 @@ class Solution:
             if nums[i] != val:
                 nums[pos] = nums[i]
                 pos += 1
-        return  pos
+        return pos
 
     def searchInsert(self, nums: List[int], target: int) -> int:
         # brute force
-        '''
+        """
         lens = len(nums)
         for i in range(lens):
             if nums[i] >= target:
                 return i
         return lens
-        '''
+        """
         # <=
         lens = len(nums)
         left, right = 0, lens
         # binary search template
         # right length, not equal, remove which median
         while left < right:
-            mid = left + (right - left)//2
+            mid = left + (right - left) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                left = mid+1
+                left = mid + 1
             else:
                 # keep right boundary
                 right = mid
@@ -81,7 +82,7 @@ class Solution:
 
     def maxSubArray(self, nums: List[int]) -> int:
         # no max value
-        '''
+        """
         lens = len(nums)
         if lens == 0:
             return 0
@@ -101,7 +102,7 @@ class Solution:
 
             i += 1
         return ans
-        '''
+        """
 
         # answer
         ans = nums[0]
@@ -139,13 +140,13 @@ class Solution:
         return digits
         '''
         # increment directly
-        if digits[lens-1] < 9:
-            digits[lens-1] += 1
+        if digits[lens - 1] < 9:
+            digits[lens - 1] += 1
         else:
             # special circumstances
             incre = 1
-            for i in range(lens-1, 0, -1):
-                if digits[i]+incre > 9:
+            for i in range(lens - 1, 0, -1):
+                if digits[i] + incre > 9:
                     digits[i] = 0
                 else:
                     digits[i] += incre
@@ -163,7 +164,7 @@ class Solution:
 
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         i, j = 0, 0
-        lens = m+n
+        lens = m + n
         # special circumstances
         if n == 0:
             return
@@ -176,8 +177,8 @@ class Solution:
             # whether i is larger than j
             if nums1[i] >= nums2[j]:
                 # exchange position
-                for k in range(lens-1,i,-1):
-                    nums1[k] = nums1[k-1]
+                for k in range(lens - 1, i, -1):
+                    nums1[k] = nums1[k - 1]
                 # insert value
                 nums1[i] = nums2[j]
                 j += 1
@@ -186,7 +187,7 @@ class Solution:
             i += 1
 
         # larger than all value
-        if j < n and nums2[j] > nums1[m-1]:
+        if j < n and nums2[j] > nums1[m - 1]:
             while j < n:
                 nums1[i] = nums2[j]
                 i += 1
@@ -195,7 +196,7 @@ class Solution:
 
     def generate(self, numRows: int) -> List[List[int]]:
         # public answer
-        '''
+        """
         triangle = []
 
         for row_num in range(numRows):
@@ -212,18 +213,18 @@ class Solution:
             triangle.append(row)
 
         return triangle
-        '''
+        """
         lists = []
         # 仅将第一位作为起始初始化，后续皆可循环推导
         if numRows >= 1:
             lists.append([1])
-        for i in range(1, numRows):
+        for _ in range(1, numRows):
             # 保存先前列表便于操作
             pre = lists[-1]
             cur_sort = [1]
             lens = len(pre)
             for j in range(1, lens):
-                cur_sort.append(pre[j] + pre[j-1])
+                cur_sort.append(pre[j] + pre[j - 1])
             cur_sort.append(1)
             lists.append(cur_sort)
 
@@ -234,29 +235,30 @@ class Solution:
         if rowIndex >= 0:
             lists.append(1)
 
-        for i in range(0, rowIndex):
+        for _ in range(0, rowIndex):
             cur = [1]
             pre = lists
             lens = len(pre)
             for j in range(1, lens):
-                cur.append(pre[j] + pre[j-1])
+                cur.append(pre[j] + pre[j - 1])
             cur.append(1)
             lists = cur
         return lists
 
+
 if __name__ == '__main__':
     show = Solution()
-    print(show.removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+    print(show.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
 
-    print(show.removeElement([0,1,2,2,3,0,4,2], 2))
+    print(show.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
 
-    print(show.searchInsert([1,3,5,6], 7))
+    print(show.searchInsert([1, 3, 5, 6], 7))
 
-    print(show.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+    print(show.maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]))
 
-    print(show.plusOne([9,9,9,9]))
+    print(show.plusOne([9, 9, 9, 9]))
 
-    print(show.merge([0],0,[1],1))
+    print(show.merge([0], 0, [1], 1))
 
     print(show.generate(5))
 
