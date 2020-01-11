ffe7448e79c119211585b4f383435ced374f5dee
Lab <cqputyang@163.com>
2020-01-11 17:26:44 +0800

    add method of 101

--- leetcode/tree/simple.py
+++ leetcode/tree/simple.py
@@ -16,8 +16,8 @@ def generate_tree(ele: List[int], pos: int) -> TreeNode:
     T = TreeNode(ele[pos])
     # 创建左右节点的下标需要注意
     # 之前一直有问题就是出在下标的计算上
-    T.left = generate_tree(ele, 2*pos+1)
-    T.right = generate_tree(ele, 2*pos+2)
+    T.left = generate_tree(ele, 2 * pos + 1)
+    T.right = generate_tree(ele, 2 * pos + 2)
     return T
 
 
@@ -46,7 +46,33 @@ class Solution:
         return res1 & res2
 
     def isSymmetric(self, root: TreeNode) -> bool:
-        pass
+        # 中序遍历结果思路有问题
+        """
+        def get_val(roots: TreeNode, arr: List[int]):
+            if roots is None:
+                arr.append(None)
+                return None
+            get_val(roots.left, arr)
+            arr.append(roots.val)
+            get_val(roots.right, arr)
+
+        arrs = []
+        get_val(root, arrs)
+        return arrs[:arrs.index(root.val)] == arrs[len(arrs)-1:arrs.index(root.val):-1]
+        """
+
+        def judge(T1: TreeNode, T2: TreeNode) -> bool:
+            # 首先判断结构是否一致
+            if T2 == T1:
+                return True
+            # 再此基础上判断值是否相等
+            if (T1 is None or T2 is None) or T1.val != T2.val:
+                return False
+
+            # 将结果汇总即可
+            return judge(T1.left, T2.right) and judge(T1.right, T2.left)
+
+        return judge(root.left, root.right) if root else True
 
 
 if __name__ == '__main__':
@@ -56,4 +82,4 @@ if __name__ == '__main__':
     # print(show.isSameTree(generate_tree([1,2,3,4], 0), generate_tree([1,2,3,None,4], 0)))
 
     # 101 对称二叉树
-    print(show.isSymmetric(generate_tree([1,2,2,3,4,4,3], 0)))
+    # print(show.isSymmetric(generate_tree([1,2,3], 0)))
