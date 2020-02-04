490e452892ea4d2e9781119c219f5d72ce423332
developer20161104 <1953256563@qq.com>
2020-02-04 11:25:58 +0800

    add method of 205

--- leetcode/hash/simple.py
+++ leetcode/hash/simple.py
@@ -28,9 +28,47 @@ class Solution:
             cur ^= i
         return cur
 
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        # 通过将字符映射到字典上，但是过于消耗内存
+        # 并且由于对排序算法的使用，时间复杂度为O(n*log n)
+        """
+        from collections import defaultdict
+        dict_1 = defaultdict(list)
+        dict_2 = defaultdict(list)
+        for i in range(len(s)):
+            dict_1[s[i]].append(i)
+            dict_2[t[i]].append(i)
+
+        return sorted(dict_1.values()) == sorted(dict_2.values())
+        """
+        # 其实只需要将其映射到数字排列即可
+        # 时间复杂度为O(n)
+        def get_centralstr(st: str) -> str:
+            # 构建映射关系字典
+            maps = [0]*120
+            ans = ""
+            count = 1
+            for ch in st:
+                pos = ord(ch)-ord('a')
+                # 对未存在于其中的构建映射
+                if not maps[pos]:
+                    maps[pos] = count
+                    count += 1
+                # 逐一添加关系
+                ans += str(maps[pos])
+
+            return ans
+
+        # 由于python内建函数耗时太可怕，时间惨不忍睹
+        return get_centralstr(s) == get_centralstr(t)
+        # 相比之下直接映射似乎时间更短
+
 
 if __name__ == '__main__':
     show = Solution()
 
     # 136 只出现一次的数字
     # print(show.singleNumber([4, 1, 2, 1, 2]))
+
+    # 205 同构字符串
+    # print(show.isIsomorphic("13", "42"))
