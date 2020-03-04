987a03f883258de12854c8034bddaedc50a2bab2
developer20161104 <1953256563@qq.com>
2020-03-04 12:36:13 +0800

    add method of 15

--- leetcode/array/array_medium.py
+++ leetcode/array/array_medium.py
@@ -18,9 +18,86 @@ class Solution:
 
         return max_area
 
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # 超时可还行，主要是每步都需要从前向后遍历，需要剪枝
+        # sort_arr = sorted(nums)
+        # lens = len(nums)
+        # left, right = 0, lens - 1
+        #
+        # res = []
+        # while left < right:
+        #     cur_sum = sort_arr[left] + sort_arr[right]
+        #     l, r = left + 1, right - 1
+        # 内部使用二分思路是没错
+        #     while l <= r:
+        #         mid = l + (r - l) // 2
+        #         cur_find = sort_arr[mid]
+        #         if not cur_find + cur_sum and [sort_arr[left], sort_arr[right], sort_arr[mid]] not in res:
+        #             res.append([sort_arr[left], sort_arr[right], sort_arr[mid]])
+        #         elif cur_find + cur_sum < 0:
+        #             l = mid + 1
+        #         else:
+        #             r = mid - 1
+        #
+        #     if sort_arr[right-1] + cur_sum < 0:
+        #         left += 1
+        #         right = lens-1
+        #     else:
+        #         right -= 1
+        #
+        # return list(res)
+
+        # 大神解法
+        # 不使用二分查找，逐一进行比对
+        # 左边低值右边高值，以及中间的值进行寻找
+        lens = len(nums)
+        res = []
+        nums.sort()
+
+        if lens < 3:
+            return res
+
+        # 固定最左边的值，从后续元素中查找（左小右大）
+        for i in range(lens):
+            # 剪枝一：如果左边已经大于0，最终结果不可能为0
+            if nums[i] > 0:
+                return res
+
+            # 过滤重复
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            # 初值设置
+            L = i+1
+            R = lens-1
+            # 每次查找的思路不同：
+            while L < R:
+                if nums[i] + nums[L] + nums[R] == 0:
+                    res.append([nums[i], nums[L], nums[R]])
+
+                    # 内部虑重
+                    while L < R and nums[L] == nums[L+1]:
+                        L += 1
+                    while L < R and nums[R] == nums[R-1]:
+                        R -= 1
+                    # 需要两边都增加，过滤掉当前满足的两个值
+                    L += 1
+                    R -= 1
+
+                # 根据求解来移动指针
+                elif nums[i] + nums[L] + nums[R] > 0:
+                    R -= 1
+                else:
+                    L += 1
+
+        return res
+
 
 if __name__ == '__main__':
     show = Solution()
 
     # 11 盛最多水的容器
     # print(show.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+
+    # 15 三数之和
+    # print(show.threeSum([-4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6]))
